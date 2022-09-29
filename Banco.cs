c6ebbb1e5d6de97feb0010243c0de8a775c1b1c2
Leonardo Rebello <leonardorebellos@gmail.com>
2022-09-29 11:35:46 -0300

    Exercício C# Bytebank

---
+++ Banco.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using bytebank;
+class Banco
+
+{   static void Main(string[] args)
+    {
+        ContaCorrente conta1 = new ContaCorrente();
+        conta1.titular = "André Silva";
+        conta1.conta = "10123-X";
+        conta1.numero_agencia = 23;
+        conta1.nome_agencia = "Agência Central";
+        conta1.saldo = 100;
+
+        ContaCorrente conta2 = new ContaCorrente();
+        conta2.titular = "Amanda Nogueira";
+        conta2.conta = "10106-X";
+        conta2.numero_agencia = 23;
+        conta2.nome_agencia = "Agência Central";
+        conta2.saldo = 100;
+
+        conta1.ExibirDados();
+        Console.WriteLine();
+
+        Console.WriteLine("Saldo de André pré transferência: " + conta1.saldo);
+        Console.WriteLine("Saldo de Amanda pré transferência: "+ conta2.saldo);
+        conta1.Transferir(50, conta2);
+        Console.WriteLine("Saldo de André após transferência: "+ conta1.saldo);
+        Console.WriteLine("Saldo de Amanda após transferência: " + conta2.saldo);
+
+        Console.ReadKey();
+
+
+    }
+}
---
+++ ContaCorrente.cs
@@ -0,0 +1,57 @@
+﻿using System; 
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+       public string titular;
+       public string conta;
+        public int numero_agencia;
+        public string nome_agencia;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if(valor < saldo)
+            {
+                return false;
+            }
+            if(valor < 0 )
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (valor > saldo)
+            {
+                return false;
+            }
+            if(valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+
+        }
+        public void ExibirDados() 
+        { 
+            Console.WriteLine("Titular: " + titular);
+            Console.WriteLine("Conta: " + conta);
+            Console.WriteLine("Número Agencia: " + numero_agencia);
+            Console.WriteLine("Nome da Agencia: " + nome_agencia);
+            Console.WriteLine("Saldo: " + saldo);
+
+        }
+    }
+}
