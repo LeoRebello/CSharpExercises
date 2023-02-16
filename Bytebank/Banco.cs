c92255f63aa7786f4f993b6c19c46fbeb8c42897
Leonardo Rebello dos Santos <leonardorebellos@gmail.com>
2023-02-16 12:16:23 -0300

    Criada pasta para o Bytebank

---
+++ Bytebank/Banco.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using bytebank;
+using bytebank.titular;
+class Banco
+
+{   static void Main(string[] args)
+    {
+       
+        ContaCorrente cliente1 = new ContaCorrente("10123 -X", 23, "Agência Central");
+        cliente1.titular = new Cliente("André Silva", "15824104 - 31", "Programador");
+        cliente1.Saldo = 100;
+        //Como trazer o titular para o método construtor ContaCorrente?
+
+        ContaCorrente cliente2 = new ContaCorrente("10106-X", 23, "Agência Central");
+        cliente2.titular = new Cliente("Amanda Santos", "748392112-21", "Médica");
+        cliente2.Saldo = 100;
+
+        cliente1.ExibirDados();
+        Console.WriteLine();
+
+        Console.WriteLine("Saldo de André pré transferência: " + cliente1.Saldo);
+        Console.WriteLine("Saldo de Amanda pré transferência: "+ cliente2.Saldo);
+        cliente1.Transferir(50, cliente2);
+        Console.WriteLine("Saldo de André após transferência: "+ cliente1.Saldo);
+        Console.WriteLine("Saldo de Amanda após transferência: " + cliente2.Saldo);
+
+        Console.WriteLine("Total de contas criadas: " + ContaCorrente.TotalDeContas);
+
+        Console.ReadKey();
+
+
+    }
+}
---
+++ Bytebank/ContaCorrente.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using bytebank.titular;
+
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+
+        public static int TotalDeContas { get; private set; }
+        public string Conta { get; set; }
+        public int Numero_agencia { get; private set; }
+        public string Nome_agencia { get; set; }
+        private double saldo;
+        public double Saldo
+        {
+            get { return saldo; }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                else
+                {
+                    this.saldo = value;
+                }
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor < saldo)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (valor > saldo)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Titular: " + titular);
+            Console.WriteLine("Conta: " + Conta);
+            Console.WriteLine("Número Agencia: " + Numero_agencia);
+            Console.WriteLine("Nome da Agencia: " + Nome_agencia);
+            Console.WriteLine("Saldo: " + Saldo);
+
+        }
+
+        //Método Construtor:
+        public ContaCorrente(string conta, int numero_agencia, string nome_agencia)
+        {
+        this.Numero_agencia = numero_agencia;
+            this.Conta = conta;
+            this.Nome_agencia = nome_agencia;
+            TotalDeContas++;
+        }
+    }
+}
+     
